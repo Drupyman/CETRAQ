0c05f294d0a5cca4f227acb6ec707bc97dfd0f29
Drupyman <114380479+Drupyman@users.noreply.github.com>
2025-10-20 23:05:40 -0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './REGISTRO CETRAQ'; // Importa tu componente principal
+// Puedes comentar estas líneas si no tienes el archivo.
+// import './index.css'; 
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
