d6fdc49fa10fa1b1230b9a6613db73e8fdf41000
Drupyman <114380479+Drupyman@users.noreply.github.com>
2025-10-21 00:36:51 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// Importa tu componente principal
+import RegistroCetraq from './components/REGISTRO CETRAQ'; 
+
+const App = () => {
+    return (
+        <RegistroCetraq />
+    );
+};
+
+export default App;
